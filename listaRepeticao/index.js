c00481b81655fbc397b646a1e03706c15006c7dc
Matheus Pasti <117304327+MPasti@users.noreply.github.com>
2023-05-22 22:15:53 -0300

    Update index.js

--- listaRepeticao/index.js
+++ listaRepeticao/index.js
@@ -124,3 +124,53 @@ function exe10(){
 console.log(`Soma dos números pares: ${par}`);
 console.log(`Soma dos números primos:  ${primo}`);
 }
+function exe21(){
+    let cand1 = 0, cand2 = 0, cand3 = 0, cand4 = 0, nulos = 0, brancos = 0;
+    let total, opcao;
+    do{
+        opcao = Number(prompt('Escolha uma opção: \n 1. Cand 1 \n 2. Cand 2 \n 3. Cand 3 \n 4. Cand 4 \n 5. Nulo \n 6. Branco \n 0. Sair do programa'))
+        switch(opcao){
+            case 1: cand1++; break
+            case 2: cand2++; break
+            case 3: cand3++; break
+            case 4: cand4++; break
+            case 5: nulos++; break
+            case 6: brancos++; break
+            case 0: alert('VOtação encerrada'); break
+            default: alert('Opção inválida')
+        }
+    }
+    while (opcao != 0)
+    total = cand1 + cand2 + cand3 + cand4 + nulos + brancos
+    if (total > 0){
+        alert(`% de votos nulos ${nulos/total*100}`)
+        alert(`% de votos brancos ${brancos/total*100}`)
+    }
+    else alert('Nenhum voto computado')   
+}
+
+function exe23(){
+    let salario, novo, ferias, decimoTerceiro, meses, opcao
+    do{
+        opcao = Number(prompt("Escolha uma opcao: \n 1. Novo salario \n 2. Ferias \n 3. Decimo terceiro \n 4. Sair"))
+    
+    switch(opcao){
+        case 1: salario = Number(prompt("Informe o salario"))
+                if((salario >=0) && (salario < 210)){
+                    novo = salario + salario*15/100
+                }
+                else if ((salario >= 210) && (salario<=600)){
+                    novo = salario + salario*10/100
+                }
+                else if(salario > 600){
+                    novo = salario + salario*5/100
+                }
+                else{
+                    alert("Salario negativo"); break
+                }
+                alert (`Novo salario ${novo}`); break 
+                
+        case 2:
+        }
+    }
+}
