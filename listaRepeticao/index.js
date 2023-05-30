ce1b4f1a8d91e8332223536bf34fb726663aecdd
Matheus Pasti <117304327+MPasti@users.noreply.github.com>
2023-05-29 21:31:53 -0300

    Update index.js

--- listaRepeticao/index.js
+++ listaRepeticao/index.js
@@ -1,3 +1,24 @@
+function exe1() {
+    let conta = 1; let conta2 = 1
+    let A, B, C, D, aux
+    while (conta2 <= 5){
+        A = Number(prompt('Informe A'))
+        B = Number(prompt('Informe B'))
+        C = Number(prompt('Informe C'))
+        D = Number(prompt('Informe D'))
+        let conta = 1
+        while (conta <= 4){
+            if (A>B) { aux = A; A = B; B = aux;}
+            if (B>C) { aux = B; B = C; C = aux;}
+            if (C>D) { aux = C; C = D; D = aux;}
+            conta++
+        }
+        alert(`Crescente ${A}, ${B}, ${C} e ${D}`)
+        alert(`Decrescente ${D}, ${C}, ${B} e ${A}`)
+        conta2++
+    }
+}
+
 function exe2(){
     let preco = 5
     let qtde = 120
@@ -13,6 +34,34 @@ function exe2(){
     document.getElementById("tabela").innerHTML = saida
 }
 
+function exe3() {
+    let idade;
+    let f1 = 0; let f2 = 0; let f3 = 0; let f4 = 0; let f5 = 0;
+    let conta = 1 
+    while (conta <=8) {
+        idade = Number(prompt(`Informe a idade ${conta}`))
+        if ((idade >= 0) && (idade <= 15)){
+            f1++ 
+        }
+        else if ((idade >= 16) && (idade <= 30)){
+            f2++
+        }
+        else if ((idade >= 31) && (idade <= 45)){
+            f3++
+        }
+        else if ((idade >= 46) && (idade <= 60)){
+            f4++
+        }
+        else if (idade > 60){
+            f5++
+        }
+        conta++ 
+    }
+   
+    alert(`Fx1: ${f1} Fx2: ${f2} Fx3: ${f3} Fx4: ${f4} Fx5: ${f5}`)
+    alert(`% Fx1: ${f1/8*100} %Fx5: ${f5/8*100}`)
+}
+
 function exe4(){
     let tabu = Number(document.getElementById("tabu").value)
     let n = 1
@@ -24,6 +73,44 @@ function exe4(){
     }
     saida = saida + "</table>"
     document.getElementById("tabela").innerHTML = saida
+}
+
+function exe5() {
+    let conta1 = 1
+    while (conta1 <= 10){
+        let conta2 = 0
+        let saida = "" 
+        while (conta2 <= 10){
+            saida = saida + `\n ${conta1} x ${conta2} = ${conta1*conta2}` 
+            conta2++
+        }
+        alert(saida)
+        conta1 ++
+    }
+}
+
+function exe6() {
+    let codigo
+    let valor; let totalVista = 0;
+    let conta = 1; let totalPrazo = 0;
+    while (conta <= 5){
+        codigo = prompt('Informe P ou V').toUpperCase()
+        valor = Number(prompt('Informe valor'))
+        if (codigo == 'P'){
+            totalPrazo += valor
+        }
+        else if (codigo == 'V'){
+            totalVista += valor
+        }
+        else {
+            alert('Código inválido, valor não será considerado')
+            continue 
+        }
+        conta++
+    }
+    let total = totalVista + totalPrazo
+    let valorParcela = totalPrazo / 3
+    document.getElementById("result6").innerHTML = `Total à vista ${totalVista} Total à prazo ${totalPrazo} Total ${total} Primeira parcela ${valorParcela.toFixed(2)}`
 }
 
 function exe07(){
@@ -49,6 +136,7 @@ function exe07(){
     console.log("Media da altura das pessoas entre 10 e 20 anos: ", media)
     console.log("Porcentagem de pessoas com peso inferior a 40kg: ", porc)
 }
+
 function exe08(){
     let idade, peso, altura, olhos, q, cabelo, a, midade, o, r;
     for(let i = 0; i >= 6; i++){
@@ -124,65 +212,100 @@ function exe10(){
 console.log(`Soma dos números pares: ${par}`);
 console.log(`Soma dos números primos:  ${primo}`);
 }
+
+
+function exe20() {
+    let opcao, n1, n2, n3, p1, p2, p3, media
+
+    do {
+        opcao = Number (prompt("Menu de opções. \n1. Média aritimetica \n2. Média ponderada \n3. Sair"))
+        switch (opcao) {
+            case 1: alert("Opção 1 escolhida.")
+            n1 = Number (prompt("Entre com a nota 1: "))
+            n2 = Number (prompt("Entre com a nota 2: "))
+            n3 = Number (prompt("Entre com a nota 3: "))
+            media = ((n1 + n2 + n3)/3)
+            alert(`A média do aluno foi ${media}`); break
+
+            case 2: alert("Opção 2 escolhida.")
+            n1 = Number (prompt("Entre com a nota 1: "))
+            p1 = Number (prompt("Entre com o peso da nota 1: "))
+            n2 = Number (prompt("Entre com a nota 2: "))
+            p2 = Number (prompt("Entre com o peso da nota 2: "))
+            n3 = Number (prompt("Entre com a nota 3: "))
+            p3 = Number (prompt("Entre com o peso da nota 3: "))
+            media = (((n1 * p1) + (n2 * p2) + (n3 * p3))/(p1 + p2 + p3))
+            alert(`A média do aluno foi ${media}`); break
+
+            case 3: alert("Programa encerrado"); break
+
+            default: alert("Opção inválida")
+        }
+    } while (opcao != 3)
+}
+
 function exe21(){
-    let cand1 = 0, cand2 = 0, cand3 = 0, cand4 = 0, nulos = 0, brancos = 0;
+    let c1 = 0, c2 = 0, c3 = 0, c4 = 0, nulos = 0, brancos = 0;
     let total, opcao;
     do{
         opcao = Number(prompt('Escolha uma opção: \n 1. Cand 1 \n 2. Cand 2 \n 3. Cand 3 \n 4. Cand 4 \n 5. Nulo \n 6. Branco \n 0. Sair do programa'))
         switch(opcao){
-            case 1: cand1++; break
-            case 2: cand2++; break
-            case 3: cand3++; break
-            case 4: cand4++; break
+            case 1: c1++; break
+            case 2: c2++; break
+            case 3: c3++; break
+            case 4: c4++; break
             case 5: nulos++; break
             case 6: brancos++; break
-            case 0: alert('VOtação encerrada'); break
+            case 0: alert('Votação encerrada'); break
             default: alert('Opção inválida')
         }
     }
     while (opcao != 0)
-    total = cand1 + cand2 + cand3 + cand4 + nulos + brancos
+    total = c1 + c2 + c3 + c4 + nulos + brancos
     if (total > 0){
         alert(`% de votos nulos ${nulos/total*100}`)
         alert(`% de votos brancos ${brancos/total*100}`)
     }
-    else alert('Nenhum voto computado')   
+    else alert('Nenhum voto calculado')   
 }
 
-function exe23(){
-    let salario, novo, ferias, decimoTerceiro, meses, opcao
-    do{
-        opcao = Number(prompt("Escolha uma opcao: \n 1. Novo salario \n 2. Ferias \n 3. Decimo terceiro \n 4. Sair"))
-    
-    switch(opcao){
-        case 1: salario = Number(prompt("Informe o salario"))
-                if((salario >=0) && (salario < 210)){
-                    novo = salario + salario*15/100
-                }
-                else if ((salario >= 210) && (salario<=600)){
-                    novo = salario + salario*10/100
-                }
-                else if(salario > 600){
-                    novo = salario + salario*5/100
-                }
-                else{
-                    alert("Salario negativo"); break
-                }
-                alert (`Novo salario ${novo}`); break 
-                
-        case 2:
-        case 3: salario = Number(prompt("Informe salario"))
-                meses = Number(prompt("Informe meses"))
-                if((meses >=1)&&(meses <=12)){
-                    decimoTerceiro = salario + salario ^ meses/12
-                    alert(`Decimo terceiro ${decimoTerceiro}`)
-                }else{
-                    alert(`nro de meses invalido`)
-                }
-                break
-            case 4: alert(`Encerre o programa`); break
-            default: alert(`Opção invalida`); break
+function exe23() {
+    let salario, novo, ferias, dt, meses, opcao
+
+    do {
+        opcao = Number (prompt(`Escolha uma opção: \n1. Novo salário \n2. Férias \n3. Décimo Terceiro \n4. Sair`))
+        switch (opcao){
+            case 1: salario = Number (prompt(`Informe o salário.`))
+            if ((salario >= 0) && (salario < 210)){
+                novo = salario + salario*0.15
+            }
+            else if ((salario >= 210) && (salario <= 600)){
+                novo = salario + salario*0.1
+            }
+            else if (salario > 600){
+                novo = salario + salario*0.05
+            }
+            else{
+                alert(`Salário negativo`); break
+            }
+            alert(`O novo salário será ${novo}`); break
+
+            case 2: salario = Number (prompt(`Informe o salário.`))
+            ferias = salario + salario*0.33
+            alert(`Valor de férias: ${ferias}`)
+
+            case 3: salario = Number (prompt(`Informe o salário.`))
+                    meses = Number (prompt(`Informe os meses.`))
+                    if ((meses >= 1) && (meses <= 12)) {
+                        dt = salario + salario*meses/12
+                        alert(`Décimo terceiro: ${dt}`)
+                    }
+                    else {
+                        alert(`Nro de meses inválido`)
+                    }
+                    break
+            case 4: alert(`Programa encerrado.`); break
+            default: alert(`Opção inválida.`)
         }
-    }
-    while(opcao != 4)
+    } while (opcao != 4)
 }
